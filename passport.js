3d9e8e94f5a9fed75cd3877147a18ae77f513a89
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-03-10 23:31:34 +0800

    passport strategy for logging in and authorize edit requests

---
+++ passport.js
@@ -0,0 +1,47 @@
+const passport = require("passport");
+const passportJWT = require("passport-jwt");
+const LocalStrategy = require("passport-local").Strategy;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const bcrypt = require("bcryptjs");
+
+const Author = require("./models/author");
+
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    Author.findOne({ username: username }, function(err, author) {
+      if (err) {
+        return done(err);
+      }
+      if (!author) {
+        return done(null, false, { message: "incorrect username" });
+      }
+      bcrypt.compare(password, author.password, (err, res) => {
+        if (res) {
+          return done(null, author);
+        } else {
+          return done(null, false, { message: "incorrect password" });
+        }
+      });
+    });
+  })
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SEC
+    },
+    function(payload, done) {
+      Author.findById(payload._id, (err, author) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (author) {
+          return done(null, author);
+        }
+      });
+    }
+  )
+);
